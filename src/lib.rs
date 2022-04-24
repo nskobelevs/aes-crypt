bc6e8f5a0e5d972b46b20fe4f91b05d9f4c661b3
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2022-04-24 02:58:34 +0100

    Modify key expansion to return flat array
    Since each round uses as 128-bit key regardless of AES type, the
    key expansion initally returned an array of those keys (`[[u8; 5]; Nr]`).
    The conversion of a flat array to said type had an affect on
    performance so it's been converted to just return the flat array now.
    
    Benchmarks after change:
    
    ```
    subword                 time:   [1.2762 ns 1.2769 ns 1.2776 ns]
                            change: [-0.6448% -0.4478% -0.2721%] (p = 0.00 < 0.05)
                            Change within noise threshold.
    
    expand 128 bit key      time:   [72.038 ns 72.084 ns 72.133 ns]
                            change: [-10.677% -10.511% -10.337%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    expand 196 bit key      time:   [138.62 ns 138.71 ns 138.80 ns]
                            change: [-11.326% -11.089% -10.846%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    expand 256 bit key      time:   [166.91 ns 167.01 ns 167.12 ns]
                            change: [-8.2234% -7.9818% -7.7311%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    AES-128                 time:   [301.88 ns 302.06 ns 302.26 ns]
                            change: [-6.1205% -5.2904% -4.5017%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    AES-196                 time:   [401.70 ns 401.80 ns 401.91 ns]
                            change: [-9.9982% -9.7713% -9.5431%] (p = 0.00 < 0.05)
                            Performance has improved.
    
    AES-256                 time:   [482.10 ns 482.32 ns 482.60 ns]
                            change: [-13.919% -12.779% -11.777%] (p = 0.00 < 0.05)
                            Performance has improved.
    ```
    
    The change causes a 10.07% improvement in key expansion speed and
    a 9.28% improvement in encryption speed.

--- src/lib.rs
+++ src/lib.rs
@@ -69,11 +69,11 @@ pub const fn SubWord(word: u32) -> u32 {
 }
 
 macro_rules! create_aes_functions {
-    ( $key_length:literal, $key_words_count:literal, $round_count:literal) => {
+    ( $key_length:literal, $key_words_count:literal, $round_count:literal, $expanded_key_size:literal) => {
         paste! {
             #[doc = concat!("Expands a ", stringify!($key_length), " bit length key for AES-", stringify!($key_length), " encryption.")]
-            pub const fn [<expand_ $key_length _bit_key>](key: [u32; $key_words_count]) -> [[u32; 4]; $round_count] {
-                let mut key_words = [0u32; $round_count * 4];
+            pub const fn [<expand_ $key_length _bit_key>](key: [u32; $key_words_count]) -> [u32; $expanded_key_size] {
+                let mut key_words = [0u32; $expanded_key_size];
 
                 let mut i = 0;
 
@@ -91,16 +91,7 @@ macro_rules! create_aes_functions {
                     i += 1;
                 }
 
-                let mut round_keys = [[0u32; 4]; $round_count];
-
-                let mut i = 0;
-
-                while i < key_words.len() {
-                    round_keys[i / 4][i % 4] = key_words[i];
-                    i += 1;
-                }
-
-                round_keys
+                key_words
             }
 
             #[doc = concat!("Performs AES-", stringify!($key_length), " on a block with a ", stringify!($key_length), "-bit key length.")]
@@ -116,7 +107,7 @@ macro_rules! create_aes_functions {
                         }
                     }
 
-                    AddRoundKey(state, expanded_key[i]);
+                    AddRoundKey(state, &expanded_key[i*4..(i+1)*4]);
                 }
             }
 
@@ -124,13 +115,13 @@ macro_rules! create_aes_functions {
     };
 }
 
-create_aes_functions!(128, 4, 11);
-create_aes_functions!(196, 6, 13);
-create_aes_functions!(256, 8, 15);
+create_aes_functions!(128, 4, 11, 44);
+create_aes_functions!(196, 6, 13, 52);
+create_aes_functions!(256, 8, 15, 60);
 
 /// Performs XOR on each byte of a state with each byte of a round key
 #[allow(non_snake_case)]
-pub fn AddRoundKey(state: &mut [u32; 4], round_key: [u32; 4]) {
+pub fn AddRoundKey(state: &mut [u32; 4], round_key: &[u32]) {
     for i in 0..state.len() {
         state[i] ^= round_key[i];
     }
@@ -215,17 +206,17 @@ mod tests {
         let w = expand_128_bit_key(key);
 
         let expected = [
-            [0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c],
-            [0xa0fafe17, 0x88542cb1, 0x23a33939, 0x2a6c7605],
-            [0xf2c295f2, 0x7a96b943, 0x5935807a, 0x7359f67f],
-            [0x3d80477d, 0x4716fe3e, 0x1e237e44, 0x6d7a883b],
-            [0xef44a541, 0xa8525b7f, 0xb671253b, 0xdb0bad00],
-            [0xd4d1c6f8, 0x7c839d87, 0xcaf2b8bc, 0x11f915bc],
-            [0x6d88a37a, 0x110b3efd, 0xdbf98641, 0xca0093fd],
-            [0x4e54f70e, 0x5f5fc9f3, 0x84a64fb2, 0x4ea6dc4f],
-            [0xead27321, 0xb58dbad2, 0x312bf560, 0x7f8d292f],
-            [0xac7766f3, 0x19fadc21, 0x28d12941, 0x575c006e],
-            [0xd014f9a8, 0xc9ee2589, 0xe13f0cc8, 0xb6630ca6],
+            0x2b7e1516, 0x28aed2a6, 0xabf71588, 0x09cf4f3c,
+            0xa0fafe17, 0x88542cb1, 0x23a33939, 0x2a6c7605,
+            0xf2c295f2, 0x7a96b943, 0x5935807a, 0x7359f67f,
+            0x3d80477d, 0x4716fe3e, 0x1e237e44, 0x6d7a883b,
+            0xef44a541, 0xa8525b7f, 0xb671253b, 0xdb0bad00,
+            0xd4d1c6f8, 0x7c839d87, 0xcaf2b8bc, 0x11f915bc,
+            0x6d88a37a, 0x110b3efd, 0xdbf98641, 0xca0093fd,
+            0x4e54f70e, 0x5f5fc9f3, 0x84a64fb2, 0x4ea6dc4f,
+            0xead27321, 0xb58dbad2, 0x312bf560, 0x7f8d292f,
+            0xac7766f3, 0x19fadc21, 0x28d12941, 0x575c006e,
+            0xd014f9a8, 0xc9ee2589, 0xe13f0cc8, 0xb6630ca6,
         ];
 
         assert_eq!(expected.len(), w.len(), "expected length of expanded key to be {0}", expected.len());
@@ -241,19 +232,19 @@ mod tests {
         let w = expand_196_bit_key(key);
 
         let expected = [
-            [0x8e73b0f7, 0xda0e6452, 0xc810f32b, 0x809079e5],
-            [0x62f8ead2, 0x522c6b7b, 0xfe0c91f7, 0x2402f5a5],
-            [0xec12068e, 0x6c827f6b, 0x0e7a95b9, 0x5c56fec2],
-            [0x4db7b4bd, 0x69b54118, 0x85a74796, 0xe92538fd],
-            [0xe75fad44, 0xbb095386, 0x485af057, 0x21efb14f],
-            [0xa448f6d9, 0x4d6dce24, 0xaa326360, 0x113b30e6],
-            [0xa25e7ed5, 0x83b1cf9a, 0x27f93943, 0x6a94f767],
-            [0xc0a69407, 0xd19da4e1, 0xec1786eb, 0x6fa64971],
-            [0x485f7032, 0x22cb8755, 0xe26d1352, 0x33f0b7b3],
-            [0x40beeb28, 0x2f18a259, 0x6747d26b, 0x458c553e],
-            [0xa7e1466c, 0x9411f1df, 0x821f750a, 0xad07d753],
-            [0xca400538, 0x8fcc5006, 0x282d166a, 0xbc3ce7b5],
-            [0xe98ba06f, 0x448c773c, 0x8ecc7204, 0x01002202],
+            0x8e73b0f7, 0xda0e6452, 0xc810f32b, 0x809079e5,
+            0x62f8ead2, 0x522c6b7b, 0xfe0c91f7, 0x2402f5a5,
+            0xec12068e, 0x6c827f6b, 0x0e7a95b9, 0x5c56fec2,
+            0x4db7b4bd, 0x69b54118, 0x85a74796, 0xe92538fd,
+            0xe75fad44, 0xbb095386, 0x485af057, 0x21efb14f,
+            0xa448f6d9, 0x4d6dce24, 0xaa326360, 0x113b30e6,
+            0xa25e7ed5, 0x83b1cf9a, 0x27f93943, 0x6a94f767,
+            0xc0a69407, 0xd19da4e1, 0xec1786eb, 0x6fa64971,
+            0x485f7032, 0x22cb8755, 0xe26d1352, 0x33f0b7b3,
+            0x40beeb28, 0x2f18a259, 0x6747d26b, 0x458c553e,
+            0xa7e1466c, 0x9411f1df, 0x821f750a, 0xad07d753,
+            0xca400538, 0x8fcc5006, 0x282d166a, 0xbc3ce7b5,
+            0xe98ba06f, 0x448c773c, 0x8ecc7204, 0x01002202,
         ];
 
         assert_eq!(expected.len(), w.len(), "expected length of expanded key to be {0}", expected.len());
@@ -269,21 +260,21 @@ mod tests {
         let w = expand_256_bit_key(key);
 
         let expected = [
-            [0x603deb10, 0x15ca71be, 0x2b73aef0, 0x857d7781],
-            [0x1f352c07, 0x3b6108d7, 0x2d9810a3, 0x0914dff4],
-            [0x9ba35411, 0x8e6925af, 0xa51a8b5f, 0x2067fcde],
-            [0xa8b09c1a, 0x93d194cd, 0xbe49846e, 0xb75d5b9a],
-            [0xd59aecb8, 0x5bf3c917, 0xfee94248, 0xde8ebe96],
-            [0xb5a9328a, 0x2678a647, 0x98312229, 0x2f6c79b3],
-            [0x812c81ad, 0xdadf48ba, 0x24360af2, 0xfab8b464],
-            [0x98c5bfc9, 0xbebd198e, 0x268c3ba7, 0x09e04214],
-            [0x68007bac, 0xb2df3316, 0x96e939e4, 0x6c518d80],
-            [0xc814e204, 0x76a9fb8a, 0x5025c02d, 0x59c58239],
-            [0xde136967, 0x6ccc5a71, 0xfa256395, 0x9674ee15],
-            [0x5886ca5d, 0x2e2f31d7, 0x7e0af1fa, 0x27cf73c3],
-            [0x749c47ab, 0x18501dda, 0xe2757e4f, 0x7401905a],
-            [0xcafaaae3, 0xe4d59b34, 0x9adf6ace, 0xbd10190d],
-            [0xfe4890d1, 0xe6188d0b, 0x046df344, 0x706c631e],
+            0x603deb10, 0x15ca71be, 0x2b73aef0, 0x857d7781,
+            0x1f352c07, 0x3b6108d7, 0x2d9810a3, 0x0914dff4,
+            0x9ba35411, 0x8e6925af, 0xa51a8b5f, 0x2067fcde,
+            0xa8b09c1a, 0x93d194cd, 0xbe49846e, 0xb75d5b9a,
+            0xd59aecb8, 0x5bf3c917, 0xfee94248, 0xde8ebe96,
+            0xb5a9328a, 0x2678a647, 0x98312229, 0x2f6c79b3,
+            0x812c81ad, 0xdadf48ba, 0x24360af2, 0xfab8b464,
+            0x98c5bfc9, 0xbebd198e, 0x268c3ba7, 0x09e04214,
+            0x68007bac, 0xb2df3316, 0x96e939e4, 0x6c518d80,
+            0xc814e204, 0x76a9fb8a, 0x5025c02d, 0x59c58239,
+            0xde136967, 0x6ccc5a71, 0xfa256395, 0x9674ee15,
+            0x5886ca5d, 0x2e2f31d7, 0x7e0af1fa, 0x27cf73c3,
+            0x749c47ab, 0x18501dda, 0xe2757e4f, 0x7401905a,
+            0xcafaaae3, 0xe4d59b34, 0x9adf6ace, 0xbd10190d,
+            0xfe4890d1, 0xe6188d0b, 0x046df344, 0x706c631e,
         ];
 
         assert_eq!(expected.len(), w.len(), "expected length of expanded key to be {0}", expected.len());
@@ -300,7 +291,7 @@ mod tests {
 
         let expanded_key = expand_128_bit_key(key);
 
-        AddRoundKey(&mut input, expanded_key[0]);
+        AddRoundKey(&mut input, &expanded_key[0..4]);
 
         let expected: [u32; 4] = [0x193de3be, 0xa0f4e22b, 0x9ac68d2a, 0xe9f84808];
 
